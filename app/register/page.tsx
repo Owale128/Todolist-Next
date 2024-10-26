78d71acf03f7a15fdd9ce7809b5e7fd9dbb3fa04
Owale128 <Christian.buaka@hotmail.com>
2024-10-26 13:42:53 +0200

    Added min length on password

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -69,6 +69,7 @@ const Register= () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         className="border-2 border-black block mb-2"
+        minLength={10}
         required
         />
 
